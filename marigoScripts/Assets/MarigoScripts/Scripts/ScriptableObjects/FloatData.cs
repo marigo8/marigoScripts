045d1cda7c0f8e8bdde3e33ec830cf4500c7278b
Mario Tijerino <mariogotijerino@gmail.com>
2020-12-03 11:36:50 -0700

    IntData and FloatData

---
+++ marigoScripts/Assets/MarigoScripts/Scripts/ScriptableObjects/FloatData.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Data/Float")]
+public class FloatData : ScriptableObject
+{
+    public float value;
+
+    public UnityEvent updateValueEvent, zeroEvent;
+
+    public void AddToValue(float amount)
+    {
+        value += amount;
+        OnUpdateValue();
+    }
+
+    public void AddToValue(FloatData data)
+    {
+        value += data.value;
+        OnUpdateValue();
+    }
+
+    public void SetValue(float amount)
+    {
+        value = amount;
+        OnUpdateValue();
+    }
+
+    public void SetValue(FloatData data)
+    {
+        value = data.value;
+        OnUpdateValue();
+    }
+
+    private void OnUpdateValue()
+    {
+        updateValueEvent.Invoke();
+        if (value <= 0f)
+        {
+            zeroEvent.Invoke();
+        }
+    }
+}
---
+++ marigoScripts/Assets/MarigoScripts/Scripts/ScriptableObjects/IntData.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Data/Int")]
+public class IntData : ScriptableObject
+{
+    public int value;
+
+    public UnityEvent updateValueEvent, zeroEvent;
+
+    public void AddToValue(int amount)
+    {
+        value += amount;
+        OnUpdateValue();
+    }
+
+    public void AddToValue(IntData data)
+    {
+        value += data.value;
+        OnUpdateValue();
+    }
+
+    public void SetValue(int amount)
+    {
+        value = amount;
+        OnUpdateValue();
+    }
+
+    public void SetValue(IntData data)
+    {
+        value = data.value;
+        OnUpdateValue();
+    }
+
+    private void OnUpdateValue()
+    {
+        updateValueEvent.Invoke();
+        if (value <= 0)
+        {
+            zeroEvent.Invoke();
+        }
+    }
+}
